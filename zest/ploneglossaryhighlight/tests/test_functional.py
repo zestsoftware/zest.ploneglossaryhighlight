320e454e59bcd1a9432abcedda137c17ee7839b4
Maurits van Rees <maurits@vanrees.org>
2011-11-24 02:20:15 +0100

    more tests

--- zest/ploneglossaryhighlight/tests/test_functional.py
+++ zest/ploneglossaryhighlight/tests/test_functional.py
@@ -4,6 +4,7 @@ from Products.CMFCore.utils import getToolByName
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 
+from zest.ploneglossaryhighlight.adapters import YES, NO, PARENT
 from zest.ploneglossaryhighlight.testing import (
     PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_INTEGRATION_TESTING,
     PLONEGLOSSARYHIGHLIGHT_INTEGRATION_TESTING,
@@ -27,6 +28,24 @@ class TestNormalGlossary(unittest.TestCase):
         new_id = portal.invokeFactory('Document', 'doc')
         self.assertTrue(gtool.highlightContent(portal[new_id]))
 
+    def testNoHighlightImage(self):
+        portal = self.layer['portal']
+        gtool = getToolByName(portal, 'portal_glossary')
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        new_id = portal.invokeFactory('Image', 'image')
+        self.assertFalse(gtool.highlightContent(portal[new_id]))
+
+    def testHighlightDocInFolder(self):
+        portal = self.layer['portal']
+        gtool = getToolByName(portal, 'portal_glossary')
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        folder_id = portal.invokeFactory('Folder', 'folder')
+        folder = portal[folder_id]
+        doc_id = folder.invokeFactory('Document', 'doc')
+        doc = folder[doc_id]
+        self.assertFalse(gtool.highlightContent(folder))
+        self.assertTrue(gtool.highlightContent(doc))
+
 
 class TestInstalled(unittest.TestCase):
 
@@ -38,9 +57,61 @@ class TestInstalled(unittest.TestCase):
         new_id = portal.invokeFactory('Document', 'doc')
         self.assertTrue(portal[new_id].getField('highlight'))
 
+    def testDefaultValue(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        new_id = portal.invokeFactory('Document', 'doc')
+        doc = portal[new_id]
+        self.assertTrue(doc.getField('highlight').get(doc), PARENT)
+
     def testHighlightDoc(self):
         portal = self.layer['portal']
         gtool = getToolByName(portal, 'portal_glossary')
         setRoles(portal, TEST_USER_ID, ('Manager',))
         new_id = portal.invokeFactory('Document', 'doc')
         self.assertTrue(gtool.highlightContent(portal[new_id]))
+
+    def testNoHighlightImage(self):
+        portal = self.layer['portal']
+        gtool = getToolByName(portal, 'portal_glossary')
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        new_id = portal.invokeFactory('Image', 'image')
+        self.assertFalse(gtool.highlightContent(portal[new_id]))
+
+    def testHighlightDocOff(self):
+        portal = self.layer['portal']
+        gtool = getToolByName(portal, 'portal_glossary')
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        new_id = portal.invokeFactory('Document', 'doc')
+        doc = portal[new_id]
+        doc.getField('highlight').set(doc, NO)
+        self.assertFalse(gtool.highlightContent(doc))
+
+    def testHighlightDocOffFolderOn(self):
+        portal = self.layer['portal']
+        gtool = getToolByName(portal, 'portal_glossary')
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        folder_id = portal.invokeFactory('Folder', 'folder')
+        folder = portal[folder_id]
+        doc_id = folder.invokeFactory('Document', 'doc')
+        doc = folder[doc_id]
+        folder.getField('highlight').set(folder, YES)
+        # The Folder type is never highlighted
+        self.assertFalse(gtool.highlightContent(folder))
+        self.assertTrue(gtool.highlightContent(doc))
+        doc.getField('highlight').set(doc, NO)
+        self.assertFalse(gtool.highlightContent(doc))
+
+    def testHighlightDocOnFolderOff(self):
+        portal = self.layer['portal']
+        gtool = getToolByName(portal, 'portal_glossary')
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        folder_id = portal.invokeFactory('Folder', 'folder')
+        folder = portal[folder_id]
+        doc_id = folder.invokeFactory('Document', 'doc')
+        doc = folder[doc_id]
+        folder.getField('highlight').set(folder, NO)
+        self.assertFalse(gtool.highlightContent(folder))
+        self.assertFalse(gtool.highlightContent(doc))
+        doc.getField('highlight').set(doc, YES)
+        self.assertTrue(gtool.highlightContent(doc))
