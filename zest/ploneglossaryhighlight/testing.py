4458ecd9045cf933da860b9e17db4e89508db206
Maurits van Rees <maurits@vanrees.org>
2011-11-24 01:52:42 +0100

    Added testing.py

---
+++ zest/ploneglossaryhighlight/testing.py
@@ -0,0 +1,36 @@
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from zope.configuration import xmlconfig
+
+
+class PloneGlossaryHighlightNotInstalled(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import zest.ploneglossaryhighlight
+        xmlconfig.file('configure.zcml', zest.ploneglossaryhighlight,
+                       context=configurationContext)
+
+
+class PloneGlossaryHighlight(PloneGlossaryHighlightNotInstalled):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'zest.ploneglossaryhighlight:default')
+
+
+PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_FIXTURE = \
+    PloneGlossaryHighlightNotInstalled()
+PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_FIXTURE,),
+    name="PloneGlossaryHighlightNotInstalled:Integration")
+
+PLONEGLOSSARYHIGHLIGHT_FIXTURE = PloneGlossaryHighlight()
+PLONEGLOSSARYHIGHLIGHT_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONEGLOSSARYHIGHLIGHT_FIXTURE,),
+    name="PloneGlossaryHighlight:Integration")
