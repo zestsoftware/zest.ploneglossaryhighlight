08ea946a2dea6c5b06b1e4704e0f3f558dc09422
Maurits van Rees <maurits@vanrees.org>
2019-04-17 22:24:54 +0200

    We do not use any properties of unittest2.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "Products.PloneGlossary>=1.5.0",
         "archetypes.schemaextender",
     ],
-    extras_require={"test": ["plone.app.testing", "unittest2"]},
+    extras_require={"test": ["plone.app.testing"]},
     entry_points="""
       # -*- Entry points: -*-
 
