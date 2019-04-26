f0868e6d423c3e6ad72ed14ac7bdbf165a54890a
Maurits van Rees <maurits@vanrees.org>
2019-04-26 19:21:54 +0200

    Added tests for Dexterity.
    Cleaned up the Archetypes tests too, making them less long.

--- zest/ploneglossaryhighlight/testing.py
+++ zest/ploneglossaryhighlight/testing.py
@@ -1,7 +1,10 @@
+from plone.app.testing import applyProfile
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import applyProfile
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from Products.CMFCore.utils import getToolByName
 from zope.configuration import xmlconfig
 
 
@@ -25,14 +28,8 @@ class PloneGlossaryHighlightNotInstalled(PloneSandboxLayer):
     def setUpPloneSite(self, portal):
         applyProfile(portal, "Products.PloneGlossary:default")
 
-
-class PloneGlossaryHighlight(PloneGlossaryHighlightNotInstalled):
-
-    defaultBases = (PLONE_FIXTURE,)
-
-    def setUpPloneSite(self, portal):
-        applyProfile(portal, "Products.PloneGlossary:default")
-        applyProfile(portal, "zest.ploneglossaryhighlight:default")
+        # We don't care about permissions in these tests.
+        setRoles(portal, TEST_USER_ID, ("Manager",))
 
 
 PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_FIXTURE = PloneGlossaryHighlightNotInstalled()
@@ -41,7 +38,65 @@ PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_INTEGRATION_TESTING = IntegrationTesting(
     name="PloneGlossaryHighlightNotInstalled:Integration",
 )
 
+
+class PloneGlossaryHighlight(PloneGlossaryHighlightNotInstalled):
+
+    defaultBases = (PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_FIXTURE,)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, "Products.PloneGlossary:default")
+        applyProfile(portal, "zest.ploneglossaryhighlight:default")
+
+
 PLONEGLOSSARYHIGHLIGHT_FIXTURE = PloneGlossaryHighlight()
 PLONEGLOSSARYHIGHLIGHT_INTEGRATION_TESTING = IntegrationTesting(
     bases=(PLONEGLOSSARYHIGHLIGHT_FIXTURE,), name="PloneGlossaryHighlight:Integration"
+)
+
+
+class PloneGlossaryHighlightDexterity(PloneGlossaryHighlight):
+
+    defaultBases = (PLONEGLOSSARYHIGHLIGHT_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # import plone.app.dexterity
+        # self.loadZCML(name='meta.zcml', package=plone.app.dexterity)
+        # self.loadZCML(package=plone.app.dexterity)
+        import plone.dexterity
+
+        self.loadZCML(name="meta.zcml", package=plone.dexterity)
+        self.loadZCML(package=plone.dexterity)
+
+    def setUpPloneSite(self, portal):
+        from plone.dexterity.fti import DexterityFTI
+
+        # Create FTI with our behavior enabled.
+        fti = DexterityFTI("enabledtype")
+        portal.portal_types._setObject("enabledtype", fti)
+        fti.klass = "plone.dexterity.content.Container"
+        fti.filter_content_types = False
+        fti.behaviors = (
+            "zest.ploneglossaryhighlight",
+            "plone.app.dexterity.behaviors.metadata.IBasic",
+        )
+
+        # Create FTI with our behavior disabled.
+        fti = DexterityFTI("disabledtype")
+        portal.portal_types._setObject("disabledtype", fti)
+        fti.klass = "plone.dexterity.content.Container"
+        fti.filter_content_types = False
+        fti.behaviors = ("plone.app.dexterity.behaviors.metadata.IBasic",)
+
+        # Enable highlighting these types by default
+        gtool = getToolByName(portal, "portal_glossary")
+        allowed = list(gtool.getAllowedPortalTypes())
+        allowed.extend(["enabledtype", "disabledtype"])
+        allowed = tuple(allowed)
+        gtool.allowed_portal_types = allowed
+
+
+PLONEGLOSSARYHIGHLIGHT_DEXTERITY_FIXTURE = PloneGlossaryHighlightDexterity()
+PLONEGLOSSARYHIGHLIGHT_DEXTERITY_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONEGLOSSARYHIGHLIGHT_DEXTERITY_FIXTURE,),
+    name="PloneGlossaryHighlightDexterity:Integration",
 )
--- zest/ploneglossaryhighlight/tests/test_dx.py
+++ zest/ploneglossaryhighlight/tests/test_dx.py
@@ -1,22 +1,27 @@
 import unittest
 
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import base_hasattr
 from Products.PloneGlossary.interfaces import IOptionalHighLight
 
 from zest.ploneglossaryhighlight.adapters import YES, NO, PARENT
 from zest.ploneglossaryhighlight.testing import (
-    PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_INTEGRATION_TESTING,
-    PLONEGLOSSARYHIGHLIGHT_INTEGRATION_TESTING,
+    PLONEGLOSSARYHIGHLIGHT_DEXTERITY_INTEGRATION_TESTING,
 )
 
 
-class BaseArchetypesTestCase(unittest.TestCase):
+class BaseDexterityTestCase(unittest.TestCase):
 
-    layer = PLONEGLOSSARYHIGHLIGHT_NOT_INSTALLED_INTEGRATION_TESTING
+    layer = PLONEGLOSSARYHIGHLIGHT_DEXTERITY_INTEGRATION_TESTING
 
-    def add_one(self, context=None, portal_type="Document"):
+    def _add_one(self, context=None, enabled=True, portal_type=None):
         if context is None:
             context = self.layer["portal"]
+        if portal_type is None:
+            if enabled:
+                portal_type = "enabledtype"
+            else:
+                portal_type = "disabledtype"
         new_id = context.invokeFactory(portal_type, "some-id")
         return context[new_id]
 
@@ -25,10 +30,13 @@ class BaseArchetypesTestCase(unittest.TestCase):
         return getToolByName(portal, "portal_glossary")
 
 
-class TestNormalGlossary(BaseArchetypesTestCase):
+class TestDexterityBehaviorDisabled(BaseDexterityTestCase):
+    def add_one(self, context=None, enabled=False, portal_type=None):
+        return self._add_one(context=context, enabled=enabled, portal_type=portal_type)
+
     def testHasNoField(self):
-        doc = self.add_one()
-        self.assertFalse(doc.getField("highlight"))
+        folder = self.add_one()
+        self.assertFalse(base_hasattr(folder, "highlight"))
 
     def testHighlightDoc(self):
         doc = self.add_one()
@@ -48,38 +56,33 @@ class TestNormalGlossary(BaseArchetypesTestCase):
         self.assertTrue(gtool.highlightContent(doc))
 
 
-class TestInstalled(BaseArchetypesTestCase):
-
-    layer = PLONEGLOSSARYHIGHLIGHT_INTEGRATION_TESTING
+class TestDexterityBehaviorEnabled(BaseDexterityTestCase):
+    def add_one(self, context=None, enabled=True, portal_type=None):
+        return self._add_one(context=context, enabled=enabled, portal_type=portal_type)
 
     def testHasField(self):
-        doc = self.add_one()
-        self.assertTrue(doc.getField("highlight"))
+        folder = self.add_one()
+        self.assertTrue(base_hasattr(folder, "highlight"))
 
     def testDefaultValue(self):
-        doc = self.add_one()
-        self.assertTrue(doc.getField("highlight").get(doc), PARENT)
+        folder = self.add_one()
+        self.assertEqual(folder.highlight, PARENT)
 
     def testHighlightDoc(self):
         doc = self.add_one()
         gtool = self.get_tool()
         self.assertTrue(gtool.highlightContent(doc))
 
-    def testNoHighlightImage(self):
-        image = self.add_one(portal_type="Image")
-        gtool = self.get_tool()
-        self.assertFalse(gtool.highlightContent(image))
-
     def testHighlightDocOff(self):
         doc = self.add_one()
         gtool = self.get_tool()
-        doc.getField("highlight").set(doc, NO)
+        doc.highlight = NO
         self.assertFalse(gtool.highlightContent(doc))
 
     def testHighlightBadValue(self):
         doc = self.add_one()
         gtool = self.get_tool()
-        doc.getField("highlight").set(doc, "hello kitty")
+        doc.highlight = "hello kitty"
         self.assertIsNone(IOptionalHighLight(doc).do_highlight())
         self.assertEqual(IOptionalHighLight(doc).do_highlight("foo"), "foo")
         # The tool will fall back to its own default.
@@ -89,35 +92,34 @@ class TestInstalled(BaseArchetypesTestCase):
 
     def testHighlightDocOffFolderOn(self):
         gtool = self.get_tool()
-        folder = self.add_one(portal_type="Folder")
+        folder = self.add_one()
         doc = self.add_one(folder)
-        folder.getField("highlight").set(folder, YES)
+        folder.highlight = YES
         self.assertTrue(IOptionalHighLight(folder).do_highlight())
-        # The Folder type is still never highlighted by the tool
-        self.assertFalse(gtool.highlightContent(folder))
+        self.assertTrue(gtool.highlightContent(folder))
         self.assertTrue(IOptionalHighLight(doc).do_highlight())
         self.assertTrue(gtool.highlightContent(doc))
-        doc.getField("highlight").set(doc, NO)
+        doc.highlight = NO
         self.assertFalse(IOptionalHighLight(doc).do_highlight())
         self.assertFalse(gtool.highlightContent(doc))
 
     def testHighlightDocOnFolderOff(self):
         gtool = self.get_tool()
-        folder = self.add_one(portal_type="Folder")
+        folder = self.add_one()
         doc = self.add_one(folder)
-        folder.getField("highlight").set(folder, NO)
+        folder.highlight = NO
         self.assertFalse(IOptionalHighLight(folder).do_highlight())
         self.assertFalse(gtool.highlightContent(folder))
         self.assertFalse(IOptionalHighLight(doc).do_highlight())
         self.assertFalse(gtool.highlightContent(doc))
-        doc.getField("highlight").set(doc, YES)
+        doc.highlight = YES
         self.assertTrue(IOptionalHighLight(doc).do_highlight())
         self.assertTrue(gtool.highlightContent(doc))
 
     def testHighlightRootParent(self):
         gtool = self.get_tool()
         doc = self.add_one()
-        doc.getField("highlight").set(doc, PARENT)
+        doc.highlight = PARENT
         self.assertIsNone(IOptionalHighLight(doc).do_highlight())
         self.assertEqual(IOptionalHighLight(doc).do_highlight("foo"), "foo")
         # Default of the tool is still true.
